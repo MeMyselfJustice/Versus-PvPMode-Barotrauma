6bdb5c2c2573d63653677bbe66a59b2321b73119
MeMselfJustice <eevvggbbeerr@gmail.com>
2024-03-05 20:25:53 +0200

    added Mara

--- Lua/Autorun/GUI/customgui.lua
+++ Lua/Autorun/GUI/customgui.lua
@@ -4,6 +4,10 @@ modPath = ...
 require("clientFunctions")
 require("monsters")
 monster = "Leviathan" -- by default
+monsterList = {}
+for name, _ in pairs(monsterDict) do
+    table.insert(monsterList, name)
+end
 
 Networking.Receive("sendPlayerList", function(message, client)
     playerList = {}
@@ -12,7 +16,6 @@ Networking.Receive("sendPlayerList", function(message, client)
     for word in msg:gmatch("([^;]+);?") do
         table.insert(playerList, word)
     end
-
     drawGUI()
 end)
 
@@ -60,7 +63,7 @@ GUI.TextBlock(GUI.RectTransform(Vector2(1, 0.08), menuList.Content.RectTransform
 local monsterDropDownFrame = GUI.Frame(GUI.RectTransform(Point(500, 30), menuList.Content.RectTransform), nil)
 local monsterChoose = GUI.DropDown(GUI.RectTransform(Point(300, 23), monsterDropDownFrame.RectTransform), "Choose your Reaper", 3, nil, false)
 monsterChoose.RectTransform.AbsoluteOffset = Point(10, 0)
-for name, hash in pairs(monsterDict) do
+for name, _ in pairs(monsterDict) do
     monsterChoose.AddItem(name, name)
 end
 monsterChoose.OnSelected = function (guiComponent, object)
@@ -94,11 +97,6 @@ playerRandomButton.OnClicked = function ()
     -- TODO add "Random" string to player choose list when clicked
 end
 
-monsterList = {}
-for name, _ in pairs(monsterDict) do
-    table.insert(monsterList, name)
-end
-
 LeviathanImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
 LeviathanSprite = Sprite(modPath .. "/images/Leviathan.png")
 LeviathanPic = GUI.Image(GUI.RectTransform(Point(300, 300), LeviathanImageFrame.RectTransform, GUI.Anchor.Center), LeviathanSprite)
@@ -106,38 +104,6 @@ LeviathanPic.RectTransform.AbsoluteOffset = Point(10, 15)
 LeviathanDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), LeviathanImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Leviathan"), nil, nil, GUI.Alignment.Center)
 LeviathanDesc.RectTransform.AbsoluteOffset = Point(340, 0)
 -- LeviathanImageFrame.Visible = not LeviathanImageFrame.Visible
-
-CharybdisImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
-CharybdisSprite = Sprite(modPath .. "/images/Charybdis.png")
-CharybdisPic = GUI.Image(GUI.RectTransform(Point(300, 300), CharybdisImageFrame.RectTransform, GUI.Anchor.Center), CharybdisSprite)
-CharybdisPic.RectTransform.AbsoluteOffset = Point(10, 15)
-CharybdisDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), CharybdisImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Charybdis"), nil, nil, GUI.Alignment.Center)
-CharybdisDesc.RectTransform.AbsoluteOffset = Point(340, 0)
-CharybdisImageFrame.Visible = not CharybdisImageFrame.Visible
-
-EndwormImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
-EndwormSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
-EndwormPic = GUI.Image(GUI.RectTransform(Point(300, 300), EndwormImageFrame.RectTransform, GUI.Anchor.Center), EndwormSprite)
-EndwormPic.RectTransform.AbsoluteOffset = Point(10, 15)
-EndwormDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), EndwormImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Endworm"), nil, nil, GUI.Alignment.Center)
-EndwormDesc.RectTransform.AbsoluteOffset = Point(340, 0)
-EndwormImageFrame.Visible = not EndwormImageFrame.Visible
-
-DoomImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
-DoomSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
-DoomPic = GUI.Image(GUI.RectTransform(Point(300, 300), DoomImageFrame.RectTransform, GUI.Anchor.Center), DoomSprite)
-DoomPic.RectTransform.AbsoluteOffset = Point(10, 15)
-DoomDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), DoomImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Doom"), nil, nil, GUI.Alignment.Center)
-DoomDesc.RectTransform.AbsoluteOffset = Point(340, 0)
-DoomImageFrame.Visible = not DoomImageFrame.Visible
-
-LatcherImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
-LatcherSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
-LatcherPic = GUI.Image(GUI.RectTransform(Point(300, 300), LatcherImageFrame.RectTransform, GUI.Anchor.Center), LatcherSprite)
-LatcherPic.RectTransform.AbsoluteOffset = Point(10, 15)
-LatcherDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), LatcherImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Latcher"), nil, nil, GUI.Alignment.Center)
-LatcherDesc.RectTransform.AbsoluteOffset = Point(340, 0)
-LatcherImageFrame.Visible = not LatcherImageFrame.Visible
 
 OverseerImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
 OverseerSprite = Sprite(modPath .. "/images/Overseer.png")
@@ -155,6 +121,46 @@ ClownheadDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), ClownheadImageF
 ClownheadDesc.RectTransform.AbsoluteOffset = Point(340, 0)
 ClownheadImageFrame.Visible = not ClownheadImageFrame.Visible
 
+MaraImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
+MaraSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
+MaraPic = GUI.Image(GUI.RectTransform(Point(300, 300), MaraImageFrame.RectTransform, GUI.Anchor.Center), MaraSprite)
+MaraPic.RectTransform.AbsoluteOffset = Point(10, 15)
+MaraDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), MaraImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Mara"), nil, nil, GUI.Alignment.Center)
+MaraDesc.RectTransform.AbsoluteOffset = Point(340, 0)
+MaraImageFrame.Visible = not MaraImageFrame.Visible
+
+DoomImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
+DoomSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
+DoomPic = GUI.Image(GUI.RectTransform(Point(300, 300), DoomImageFrame.RectTransform, GUI.Anchor.Center), DoomSprite)
+DoomPic.RectTransform.AbsoluteOffset = Point(10, 15)
+DoomDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), DoomImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Doom"), nil, nil, GUI.Alignment.Center)
+DoomDesc.RectTransform.AbsoluteOffset = Point(340, 0)
+DoomImageFrame.Visible = not DoomImageFrame.Visible
+
+CharybdisImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
+CharybdisSprite = Sprite(modPath .. "/images/Charybdis.png")
+CharybdisPic = GUI.Image(GUI.RectTransform(Point(300, 300), CharybdisImageFrame.RectTransform, GUI.Anchor.Center), CharybdisSprite)
+CharybdisPic.RectTransform.AbsoluteOffset = Point(10, 15)
+CharybdisDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), CharybdisImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Charybdis"), nil, nil, GUI.Alignment.Center)
+CharybdisDesc.RectTransform.AbsoluteOffset = Point(340, 0)
+CharybdisImageFrame.Visible = not CharybdisImageFrame.Visible
+
+EndwormImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
+EndwormSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
+EndwormPic = GUI.Image(GUI.RectTransform(Point(300, 300), EndwormImageFrame.RectTransform, GUI.Anchor.Center), EndwormSprite)
+EndwormPic.RectTransform.AbsoluteOffset = Point(10, 15)
+EndwormDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), EndwormImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Endworm"), nil, nil, GUI.Alignment.Center)
+EndwormDesc.RectTransform.AbsoluteOffset = Point(340, 0)
+EndwormImageFrame.Visible = not EndwormImageFrame.Visible
+
+LatcherImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
+LatcherSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
+LatcherPic = GUI.Image(GUI.RectTransform(Point(300, 300), LatcherImageFrame.RectTransform, GUI.Anchor.Center), LatcherSprite)
+LatcherPic.RectTransform.AbsoluteOffset = Point(10, 15)
+LatcherDesc = GUI.TextBlock(GUI.RectTransform(Point(250, 300), LatcherImageFrame.RectTransform, GUI.Anchor.Center), getDescMonster("Latcher"), nil, nil, GUI.Alignment.Center)
+LatcherDesc.RectTransform.AbsoluteOffset = Point(340, 0)
+LatcherImageFrame.Visible = not LatcherImageFrame.Visible
+
 TestImageFrame = GUI.Frame(GUI.RectTransform(Point(300, 300), menuList.Content.RectTransform), nil)
 TestSprite = Sprite(modPath .. "/images/lady_justice1.jpg")
 TestPic = GUI.Image(GUI.RectTransform(Point(300, 300), TestImageFrame.RectTransform, GUI.Anchor.Center), TestSprite)
@@ -167,6 +173,7 @@ monsterVisibilityDict = {
     ['Leviathan'] = LeviathanImageFrame,
     ['Overseer'] = OverseerImageFrame,
     ['Clownhead'] = ClownheadImageFrame, 
+    ['Mara'] = MaraImageFrame,
     ['Doom'] = DoomImageFrame,
     ['Charybdis'] = CharybdisImageFrame,
     ['Endworm'] = EndwormImageFrame,
--- Lua/monsters.lua
+++ Lua/monsters.lua
@@ -2,6 +2,7 @@ monsterDict = {
     ['Leviathan'] = "Leviathan",
     ['Overseer'] = "Overseer",
     ['Clownhead'] = "Clownhead",
+    ['Mara'] = "Mara",
     ['Doom'] = "Psilotoad",                           -- це якийсь жарт?
     ['Charybdis'] = "Charybdis",
     ['Endworm'] = "Endworm",
